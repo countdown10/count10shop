06218d6f2eb099bc09ec03f7813579068d5fd138
mixedsider <dmstjd0812@gmail.com>
2025-03-24 16:29:56 +0900

    init: 프로젝트 초기 셋팅
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

---
+++ src/main/java/xyz/tomorrowlearncamp/count10shop/Count10shopApplication.java
@@ -0,0 +1,13 @@
+package xyz.tomorrowlearncamp.count10shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Count10shopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Count10shopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/tomorrowlearncamp/count10shop/Count10shopApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tomorrowlearncamp.count10shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Count10shopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
