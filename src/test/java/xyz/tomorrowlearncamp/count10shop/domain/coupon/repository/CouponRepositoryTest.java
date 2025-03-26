f3da76b451ff00bd0a510c73ed5ba63bf5b16bc9
sjMun09 <ohoh7391@naver.com>
2025-03-26 16:29:58 +0900

    Test(쿠폰):쿠폰 리포지토리_@Lock(PESSIMISTIC_WRITE)가 실제로 적용되는지 테스트하기 위해 트랜잭션 안에서 읽고, 동시에 접근 시 대기하는지 확인. 하지만 단일 테스트 함수에서는 이런 동시성 확인은 어려워서 일단은 트랜잭션 안에서 findByIdForUpdate()가 잘 실행되는지 먼저 확인

--- src/test/java/xyz/tomorrowlearncamp/count10shop/domain/coupon/repository/CouponRepositoryTest.java
+++ src/test/java/xyz/tomorrowlearncamp/count10shop/domain/coupon/repository/CouponRepositoryTest.java
@@ -21,18 +21,14 @@ class CouponRepositoryTest {
     @Autowired
     private CouponRepository couponRepository;
 
-    @Test
-    void findByIdForUpdate() {
-
-    }
 
     @Test
-    @DisplayName("동시성 테스트 - 발급 수량 증가")
-    void addIssuedQuantitySafe() {
+    @DisplayName("쿠폰_리포지토리_쿠폰_발급_수량_증가(실패)")
+    void addIssuedQuantitySafe_fail() {
         //given
         Coupon coupon = Coupon.builder()
-                .name("lock 테스트 해볼거")
-                .content("lock 테스트 쿠폰")
+                .name("테스트")
+                .content("테스트 쿠폰")
                 .minOrderPrice(5000)
                 .discountAmount(1000)
                 .totalQuantity(5)
@@ -49,4 +45,64 @@ class CouponRepositoryTest {
         Assertions.assertThat(updateCount).isEqualTo(0);
     }
 
+    @Test
+    @DisplayName("쿠폰_리포지토리_쿠폰_발급_수량_증가(성공)")
+    void addIssuedQuantitySafe_success() {
+        //given
+        Coupon coupon = Coupon.builder()
+                .name("테스트")
+                .content("테스트 쿠폰")
+                .minOrderPrice(5000)
+                .discountAmount(1000)
+                .totalQuantity(5)
+                .issuedQuantity(2)
+                .couponStatus(CouponStatus.ACTIVE)
+                .expiredAt(LocalDateTime.now().plusDays(1))
+                .build();
+        couponRepository.save(coupon);
+
+        //when
+        int updateCount = couponRepository.addIssuedQuantitySafe(coupon.getId());
+
+        //then
+        Assertions.assertThat(updateCount).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("쿠폰_리포지토리_id_조회_실패")
+    void findByIdForUpdate_fail() {
+        // given
+        Long notExistsId = 999L;
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> {
+                    couponRepository.findByIdForUpdate(notExistsId)
+                            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 쿠폰입니다."));
+                }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 쿠폰입니다.");
+    }
+
+    @Test
+    @DisplayName("쿠폰_리포지토리_id_조회_성공")
+    void findByIdForUpdate_success() {
+        // given
+        Coupon coupon = Coupon.builder()
+                .name("락 테스트")
+                .content("락 테스트용")
+                .minOrderPrice(5000)
+                .discountAmount(1000)
+                .totalQuantity(10)
+                .issuedQuantity(1)
+                .couponStatus(CouponStatus.ACTIVE)
+                .expiredAt(LocalDateTime.now().plusDays(1))
+                .build();
+        Coupon saved = couponRepository.save(coupon);
+
+        // when
+        Coupon found = couponRepository.findByIdForUpdate(saved.getId())
+                .orElseThrow(() -> new RuntimeException("쿠폰이 존재하지 않습니다"));
+
+        // then
+        Assertions.assertThat(found.getId()).isEqualTo(saved.getId());
+    }
 }
